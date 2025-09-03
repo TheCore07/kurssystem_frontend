90a3eeeb361c052ebb969c41f7b659c9852fded6
Fabian Altaner <fabian.altaner@sympacon-ts.de>
2025-09-03 07:59:31 +0200

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     build: {
         manifest: true,
         rollupOptions: {
-            input: '/path/to/main.js',
+            input: './src/main.tsx',
         },
     },
     plugins: [
